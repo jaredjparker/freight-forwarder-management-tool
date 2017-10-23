c9349b7d69d3ecaaa77d08cd763c2ab26f068286
VIISilver <jparke71@hotmail.com>
2017-10-23 11:29:05 -0600

    Switched to Chart.js 2. Have a bar chart that works.

--- src/components/ProfitComparison/ProfitComparison.js
+++ src/components/ProfitComparison/ProfitComparison.js
@@ -1,40 +1,49 @@
 import React, { Component } from 'react';
 import './ProfitComparison.css';
-import * as d3 from 'd3';
 import logo from './../AirlineMgmt/logo.svg';
+import { Bar } from 'react-chartjs-2';
 
 class ProfitComparison extends Component {
     constructor(props) {
         super(props)
-        this.createBarChart = this.createBarChart.bind(this)
-    }
-    componentDidMount() {
-        this.createBarChart()
-    }
-    componentDidUpdate() {
-        this.createBarChart()
-    }
-    createBarChart() {
-        var cost = [30, 86, 168, 281, 303, 365];
 
-        d3.select(".chart")
-            .selectAll("div")
-            .data(cost)
-            .enter()
-            .append("div")
-            .style("width", function (d) { return d + "px"; })
-            .style('height', '20px')
-            .text(function (d) { return d; });
+        this.state = {
+            data: {
+                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                datasets: [
+                    {
+                        label: 'My First dataset',
+                        backgroundColor: '#ffffff',
+                        borderColor: 'rgba(255,99,132,1)',
+                        borderWidth: 1,
+                        hoverBackgroundColor: 'rgba(255,99,132,0.4)',
+                        hoverBorderColor: 'rgba(255,99,132,1)',
+                        data: [65, 59, 80, 81, 56, 55, 40]
+                    }
+                ]
+            }
+        }
     }
+
     render() {
         return (
             <div className="profit">
                 <div className='profitheader'>
                     <img src={logo} alt="" />
-                    <h1>Dashboard</h1>
+                    <h1>Profit Comparison</h1>
                     <a href='http://localhost:3005/auth/logout'><button className='btn'><span>Log out</span></button></a>
                 </div>
-                <div className='chart'></div>
+                <div>
+                    <h2>Bar Example (custom size)</h2>
+                    <Bar
+                        data={this.state.data}
+                        width={200}
+                        height={100}
+                        options={{
+                            maintainAspectRatio: false
+                        }}
+                    />
+                </div>
             </div>
         )
     }
